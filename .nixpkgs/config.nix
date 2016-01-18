1b7be5cc0a8f83c7bc80a27a4bb5f574c11544c5
Sean Lee <freshdried@gmail.com>
2016-01-17 21:08:40 -0500

    moved ghc stuff out of config, removed myEnvFun test

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,73 +1,78 @@
 {
   allowUnfree = true;
   packageOverrides = pkgs_: with pkgs_; {
-      all = with pkgs; buildEnv {
-        name = "all";
-        paths = [
-          # nix
-          nox
-          nix-repl
 
-          # cli programs
-          nmap_graphical
-          mplayer
-          ncdu
+    # lol clean this up
+    ghcEnv = with pkgs; buildEnv {
+      name = "ghcEnv";
+      paths = [
+        cabal-install
+        cabal2nix
 
-          # ranger
-          # uncomment when my PR gets through
-          # ranger
-          # w3m
-          # file 
-
-          # x programs
-          sxiv
-
-
-          # ricing
-          compton-git
-          rofi
-          nitrogen
-
-          # x utilities
-          xclip
-          xdg_utils
-          xorg.xev
-          xorg.xbacklight
-
-          # dev tools
-          gitAndTools.hub
-          neovim
-          emacs
-          yi
-
-          # audio
-          qjackctl
-          supercollider
-
-          # dev things
-          atom
-          go
-          gcc
-          gnumake
-          nodejs-5_x
-          electron
-
-          (haskellPackages.ghcWithHoogle
-            (haskellPackages: with haskellPackages; [
-              halive
-              stack
-              purescript
-              idris
-              stylish-haskell
-              hlint
-              hasktags
-              ghc-mod
-          ]))
-        ];
-      };
-      test = pkgs.myEnvFun {
-        name = "test";
-        buildInputs = [ cowsay ];
-      };
+        (haskellPackages.ghcWithHoogle (haskellPackages: with haskellPackages; [
+          halive
+          stack
+          purescript
+          idris
+          stylish-haskell
+          hlint
+          hasktags
+          # ghc-mod
+        ]))
+      ];
     };
+
+    all = with pkgs; buildEnv {
+      name = "all";
+      paths = [
+        # nix
+        nox
+        nix-repl
+
+        # cli programs
+        nmap_graphical
+        mplayer
+        ncdu
+
+        # ranger
+        # uncomment when my PR gets through
+        # ranger
+        # w3m
+        # file 
+
+        # x programs
+        sxiv
+
+
+        # ricing
+        compton-git
+        rofi
+        nitrogen
+
+        # x utilities
+        xclip
+        xdg_utils
+        xorg.xev
+        xorg.xbacklight
+
+        # dev tools
+        gitAndTools.hub
+        neovim
+        emacs
+        yi
+
+        # audio
+        qjackctl
+        supercollider
+
+        # dev things
+        atom
+        go
+        gcc
+        gnumake
+        nodejs-5_x
+        electron
+      ];
+    };
+  };
 }

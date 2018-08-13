fb99633ff96080de3d56d8fda19fa38dc95524d1
Sean Lee <freshdried@gmail.com>
2018-08-12 20:18:11 -0400

    vscode on mac

---
+++ mac/save.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+copyFromHome () {
+  LOCAL="$HOME/$1"
+  REMOTE="$DIR/$1"
+
+  REMOTE_PARENT=$(dirname "$REMOTE")
+  mkdir -p "$REMOTE_PARENT"
+  echo "Saving: ~/$1"
+  cp "$LOCAL" "$REMOTE"
+}
+
+copyFromHome "Library/Application Support/Code/User/settings.json"
+
